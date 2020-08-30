221397fa0870ee83cb018a45e5f603c6bb2d8c1a
BashfulBaobab <akshatjohari1@gmail.com>
2020-08-30 13:00:28 +0530

    Initial commit

---
+++ document_creator.py
@@ -0,0 +1,12 @@
+# Project DocMerge
+# Author: Akshat Johari
+
+from docx import Document
+
+def main():
+    print("Welcome to the document creation chamber.\nThis program creates .docx files with images and text, in tabular format.\n")
+    name = input("Please enter the name you want this document to be given, without the .docx extension.\n")
+    
+
+if __name__ == "__main__":
+    main()
