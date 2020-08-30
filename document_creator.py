589f4a23f2cc521bc803839c3c95263af6807c43
BashfulBaobab <akshatjohari1@gmail.com>
2020-08-30 13:17:58 +0530

    Create Document

--- document_creator.py
+++ document_creator.py
@@ -6,6 +6,13 @@ from docx import Document
 def main():
     print("Welcome to the document creation chamber.\nThis program creates .docx files with images and text, in tabular format.\n")
     name = input("Please enter the name you want this document to be given, without the .docx extension.\n")
+    create_doc(name)
+    
+def create_doc(name):
+    doc = Document()
+    title = input("Please enter the title of your document.\n")
+    p = doc.add_paragraph(title)
+    doc.save(name + ".docx")
     
 
 if __name__ == "__main__":
