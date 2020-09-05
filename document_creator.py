46c2d9cfc56195723ac09301a0ebc42f2a985c77
BashfulBaobab <akshatjohari1@gmail.com>
2020-09-05 17:46:51 +0530

    Added functionality for subheading

--- document_creator.py
+++ document_creator.py
@@ -2,6 +2,8 @@
 # Author: Akshat Johari
 
 from docx import Document
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.shared import Pt
 
 def main():
     print("Welcome to the document creation chamber.\nThis program creates .docx files with images and text, in tabular format.\n")
@@ -13,6 +15,16 @@ def create_doc(name):
     title = input("Please enter the title of your document.\n")
     p = doc.add_paragraph(title)
     doc.save(name + ".docx")
+    print("File " + name + "has been created in the current directory. Fare thee well.")
+    
+    sub = input("Does your document have a subheading?\n Y for Yes, N for No\n")
+    if sub in ("y", "Y"):
+        sub = input("Please enter your subheading.\n")
+        p = doc.add_paragraph(sub)
+        p.alignment = WD_ALIGN_PARAGRAPH.CENTER
+        p.style = doc.styles['Heading 2']
+        p.style.font.size = Pt(14)
+        p.style.paragraph_format.space_after = Pt(12)    
     
 
 if __name__ == "__main__":
